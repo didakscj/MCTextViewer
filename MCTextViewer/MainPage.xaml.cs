ebc6feb7125ed2709fa0369e2eb5d933f9750ee8
youngho Yuk <six605@gmail.com>
2012-01-07 17:15:34 +0900

    Edit Text Download panorama itempage  header

--- MCTextViewer/MainPage.xaml.cs
+++ MCTextViewer/MainPage.xaml.cs
@@ -301,7 +301,7 @@ namespace MCTextViewer
             {
                 //드롭박스 접속용 초기화
                 DropboxConnected = true;
-                App._client = new DropNetClient(DROPBOXAPPKEY, DROPBOXSCRETKEY);
+                App._client = new DropNetClient("DROPBOXAPPKEY", "DROPBOXSCRETKEY");
                 // Async
                 App._client.GetTokenAsync((userToken) =>
                 {
@@ -476,8 +476,8 @@ namespace MCTextViewer
         private void SelectedFileDownload(DropBoxDataList selectedBoxItem)
         {
             DropBoxFileDownload dropdownload = new DropBoxFileDownload();
-            String requesturl = dropdownload.getfiledownloadrequest(selectedBoxItem.Path, DROPBOXAPPKEY,
-                                        DROPBOXSCRETKEY, DropboxUserToken, DropboxScretToken);
+            String requesturl = dropdownload.getfiledownloadrequest(selectedBoxItem.Path, "DROPBOXAPPKEY",
+                                        "DROPBOXSCRETKEY", DropboxUserToken, DropboxScretToken);
             
             //MessageBox.Show(requesturl);
             //다운로드 리스트에 추가
@@ -514,7 +514,7 @@ namespace MCTextViewer
 
             //파일 다운로드용 드롭박스 클라이언트 
             //드롭박스 접속용 초기화
-            //DropNetClient _downclient = new DropNetClient(DROPBOXAPPKEY, DROPBOXSCRETKEY);
+            //DropNetClient _downclient = new DropNetClient("DROPBOXAPPKEY", "DROPBOXSCRETKEY");
             //_downclient.UserLogin = new UserLogin { Token = DropboxUserToken, Secret = DropboxScretToken };
             
            
